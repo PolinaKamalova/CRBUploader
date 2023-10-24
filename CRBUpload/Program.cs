a6791c69049c37253be9b44d013cb110cc08a027
PolinaKamalova <79357093+PolinaKamalova@users.noreply.github.com>
2023-10-24 05:17:49 -0500

    Update Program.cs

--- CRBUpload/Program.cs
+++ CRBUpload/Program.cs
@@ -1,18 +1,22 @@
-﻿using System.Data.SqlTypes;
+using System.Data.SqlTypes;
 using System.Globalization;
 using System.Reflection.PortableExecutable;
 using System.Xml;
 using static System.Net.WebRequestMethods;
 using Npgsql;
+using System.Text;
+using System;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Добрый день! Пожалуйста выберите действие из списка введя номер команды");
-        Console.WriteLine("1. Вывести курс валют на сегодня");
-        Console.WriteLine("2. Вывести курс валют за месяц");
-        Console.WriteLine("3. Выход");
+        Console.WriteLine("Good Afternoon! Please select one of the further options by entering the corresponding number");
+        Console.WriteLine("1. Add today quotes to the database");
+        Console.WriteLine("2. Add monthly quotes to the database");
+        Console.WriteLine("3. Exit");
+
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
         string choice = Console.ReadLine();
 
@@ -24,7 +28,7 @@ class Program
                 break;
             case "2":
                 var date = DateTime.Today;
-                for(int i = 0; i < 30; i++)
+                for (int i = 0; i < 30; i++)
                 {
                     UploadTodayQuotes(date.ToString("dd/mm/yyyy"));
                     date.AddDays(-1);
@@ -34,63 +38,66 @@ class Program
                 Environment.Exit(0);
                 break;
             default:
-                Console.WriteLine("Неверный выбор. Пожалуйста, попробуйте снова.");
+                Console.WriteLine("Wrong choice. Please try again");
                 break;
         }
     }
 
     public static string UploadTodayQuotes(string todayDate)
     {
-        //daily currency
-        int numCode;
-        string charCode, name, value;
-        string result = "Данные за " + todayDate + " загружены";
+        string result = "Data for " + todayDate + " has not been uploaded";
 
         string cotirovkasURL = "https://cbr.ru/scripts/XML_daily.asp?date_req=" + todayDate;
 
-        XmlTextReader reader = new XmlTextReader(cotirovkasURL);
+        // Download XML content from the URL
+        string xmlContent;
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = client.GetAsync(cotirovkasURL).Result;
+            xmlContent = response.Content.ReadAsStringAsync().Result;
+        }
+
+        // Convert Win-1251 encoded XML content to UTF-8
+        Encoding win1251 = Encoding.GetEncoding("windows-1251");
+        Encoding utf8 = Encoding.UTF8;
+        byte[] utf8Bytes = Encoding.Convert(win1251, utf8, win1251.GetBytes(xmlContent));
+        string utf8XmlContent = utf8.GetString(utf8Bytes);
+
+        // Parse XML content
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(utf8XmlContent);
+
 
         string connString = "Host=localhost;Username=postgres;Password=postgres;Database=quotes";
+
         using (NpgsqlConnection connection = new NpgsqlConnection(connString))
         {
             //read daily cotirovkas info, upload to the temporary variables to incorporate into sql command
-            while (reader.Read())
+            foreach (XmlNode node in xmlDoc.SelectNodes("//Valute"))
             {
-                switch (reader.NodeType)
-                {
-                    case XmlNodeType.Element: 
-                        if (reader.Name == "NumCode")
-                            numCode = reader.Value.toInt();
-                        if (reader.Name == "CharCode")
-                            charCode = reader.Value;
-                        if (reader.Name == "Name")
-                            name = reader.Value;
-                        if (reader.Name == "Value")
-                            value = reader.Value;
-                        break;
-                    case XmlNodeType.EndElement:
-                        if(reader.Name == "Valute")
-                        {
-                            // adding new information to the database
-                            string sql = "INSERT INTO quotes VALUES (@id, @charcode, @name, @value, @numcode, @date)";
-                            using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
-                            {
-                                command.Parameters.AddWithValue("@id", numCode + todayDate);
-                                command.Parameters.AddWithValue("@charcode", charCode);
-                                command.Parameters.AddWithValue("@name", name);
-                                command.Parameters.AddWithValue("@value", value);
-                                command.Parameters.AddWithValue("@date", DateTime.Today);
+                string numCode = node.SelectSingleNode("NumCode").InnerText;
+                string charCode = node.SelectSingleNode("CharCode").InnerText;
+                string name = node.SelectSingleNode("Name").InnerText;
+                double value = Convert.ToDouble(node.SelectSingleNode("Value").InnerText, CultureInfo.InvariantCulture);
 
-                                command.ExecuteNonQuery();
-                            }
-                        }
-                        break;
+                // adding new information to the database
+                string sql = "INSERT INTO quotes VALUES (@id, @charcode, @name, @value, @numcode, @date)";
+                using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@id", numCode + todayDate);
+                    command.Parameters.AddWithValue("@charcode", charCode);
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@value", value);
+                    command.Parameters.AddWithValue("@date", todayDate);
+
+                    command.ExecuteNonQuery();
                 }
-                
+                result = "Data for " + todayDate + " has been uploaded";
             }
         }
 
         return result;
     }
+
 }
  
